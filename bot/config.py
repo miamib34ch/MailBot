5eb65e9b2f6ed92b3c79a51546c07a218caf6706
Bogdan Polygalov <77894393+miamib34ch@users.noreply.github.com>
2024-09-13 23:33:21 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,5 +2,5 @@ IMAP_SERVER = 'imap_server'
 EMAIL_ACCOUNT = 'mail'
 EMAIL_PASSWORD = 'mail_pass'
 TELEGRAM_TOKEN = 'bot_token'
-TELEGRAM_CHANNEL = 'chanel_id'
+TELEGRAM_CHANNEL = 'channel_id'
 CHECK_TIMING_SECONDS = 300
